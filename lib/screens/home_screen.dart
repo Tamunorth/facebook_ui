415f59e2f9a104173d6326d141876abae32966fe
tamunor <daviesmanuelfavour@gmail.com>
2021-07-30 01:04:20 +0100

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:facebook_ui_clone/config/palette.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28.0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
